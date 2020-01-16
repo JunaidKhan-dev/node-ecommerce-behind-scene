c8597d2dd2ce24d6242b77d8a84c8dbe5e77bcb3
JunaidKhanEU <junaidkhan1@gmx.com>
2020-01-16 16:32:22 +0000

    css for shop products

--- routes/products/index.js
+++ routes/products/index.js
@@ -4,10 +4,7 @@ const router = express.Router()
 const productsIndexTemplate = require('../../views/products')
 const productRepo = require('../../repositories/products')
 router.get('/', async (req, res) => {
-  console.log('i run')
-
   const products = await productRepo.getAll()
-  console.log(products)
   res.send(productsIndexTemplate({ products }))
 })
 module.exports = router
